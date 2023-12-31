bcc7ee18574bd86c460e27c3dec57de99a03f873
Mạnh Đạt <tranhuudats1@gmail.com>
2023-12-31 16:56:53 +0700

    UI Client Products

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,15 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model')
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({status: "active", deleted: false})
+
+    const newProducts = products.map((item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
+        return item
+    }))
+    console.log(products)
     res.render('client/pages/products/index.pug', {
-        pageTitle: "List Products"
+        pageTitle: "List Products",
+        products: newProducts
     })
 }
