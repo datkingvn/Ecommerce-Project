f0c75f28d26047bf61d2ab1d774c068c08be4aa8
Mạnh Đạt <tranhuudats1@gmail.com>
2024-01-02 15:09:15 +0700

    :zap: Find Product Filter - Admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -36,12 +36,22 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status
     }
 
+    let keyword = ""
+
+    if (req.query.keyword) {
+        keyword = req.query.keyword
+
+        const regex = new RegExp(keyword, "i");
+        find.title = regex
+    }
+
     const products = await Product.find(find)
 
     res.render('admin/pages/products/index.pug', {
         pageTitle: "List Products",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     })
 
 }
