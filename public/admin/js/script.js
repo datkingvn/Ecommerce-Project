69d9ff36d7527aff90ff10b820cdb4d9638bea02
Mạnh Đạt <tranhuudats1@gmail.com>
2024-01-04 15:29:28 +0700

    :zap: Delete Product

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -117,3 +117,26 @@ if (formChangeMulti) {
     });
 }
 // End Form Change Multi
+
+// Delete Item
+const buttonDelete = document.querySelectorAll("[button-delete]");
+if (buttonDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+
+    buttonDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Bạn muốn xoá sản phẩm này?");
+
+            if (isConfirm) {
+                const id = button.getAttribute("data-id");
+
+                const action = `${path}/${id}?_method=DELETE`
+
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
+            }
+        });
+    });
+}
+// End Delete Item
